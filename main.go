985b42ced6915d8c660c55fc69a4d26d9781ae21
Roman Otiuskyi <roman.otiuskyi@tx.local>
2019-03-13 16:10:47 +0200

    Create Middleware type, Compose and filter funcs

---
+++ main.go
@@ -0,0 +1,62 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+)
+
+// Middleware is a composable http.HandlerFunc wrapper
+type Middleware func(next http.HandlerFunc) http.HandlerFunc
+
+// Compose http.HandlerFunc from Middleware array
+func Compose(middlewareArr ...Middleware) http.HandlerFunc {
+	return func(rw http.ResponseWriter, req *http.Request) {
+		if len(middlewareArr) == 0 {
+			return
+		}
+
+		hf := func(http.ResponseWriter, *http.Request) { /* noop */ }
+
+		lastI := len(middlewareArr) - 1
+		last, body := middlewareArr[lastI], middlewareArr[:lastI]
+
+		hf = last(hf)
+
+		for i := len(body) - 1; i >= 0; i-- {
+			hf = body[i](hf)
+		}
+
+		hf(rw, req)
+	}
+}
+
+// Post method filter
+func Post(hf http.HandlerFunc) Middleware {
+	return methodSwitch("POST", hf)
+}
+
+// Get method filter
+func Get(hf http.HandlerFunc) Middleware {
+	return methodSwitch("GET", hf)
+}
+
+// Put method filter
+func Put(hf http.HandlerFunc) Middleware {
+	return methodSwitch("PUT", hf)
+}
+
+// Delete method filter
+func Delete(hf http.HandlerFunc) Middleware {
+	return methodSwitch("DELETE", hf)
+}
+
+func methodSwitch(method string, hf http.HandlerFunc) Middleware {
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		return func(rw http.ResponseWriter, req *http.Request) {
+			if strings.ToUpper(req.Method) == method {
+				hf(rw, req)
+			}
+			next(rw, req)
+		}
+	}
+}
